e804429c955bde52c068712c8ed56371173ca749
johncurcio <joao8177@gmail.com>
2019-07-25 17:12:40 -0300

    add lessson 2 part 1

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# facebook-secure-private-ai
-Secure and Private AI Scholarship Solutions
+# Secure and Private AI Scholarship
+
+This is a repo to document my progress on the udacity facebook scholarship program: [Secure and Private AI Scholarship](https://www.udacity.com/facebook-AI-scholarship)
+
---
+++ lesson 2/01_tensors.py
@@ -0,0 +1,24 @@
+import torch
+
+def activation(x):
+	"""
+		Sigmoid activation function
+
+		Arguments
+		---------
+		x: torch.Tensor
+	"""
+	return 1/(1 + torch.exp(-x))
+
+torch.manual_seed(7) # rgn seed for randn
+
+features = torch.randn((1, 5)) # creates a random vector/tensor 1x5
+weights = torch.randn_like(features) # creates a random vector 1x5 (same size as features)
+bias = torch.randn((1, 1))
+
+# y = f(w*x + b)
+# y = activation(weights*features + bias)
+
+y = activation(torch.sum(torch.mn(features, weights.view(5, 1)), bias))
+
+print(y)
